1983aa3530bd54e737cdba38a1eb72aa6d54163d
Kandwanahonathan <nathankandwanaho3@gmail.com>
2025-10-22 19:08:31 +0200

    mi roap of chat

---
+++ my-app/src/chat-deign/chat.design.jsx
@@ -0,0 +1,17 @@
+import React,{useState,useEffect} from "react";
+const Chat=()=>{
+    return(
+        <div className="App">
+            <div className="head">
+                <h4>Live Chat</h4>
+                </div>
+
+            <div>
+                <button></button>
+            </div>
+
+
+
+        </div>
+    )
+}
