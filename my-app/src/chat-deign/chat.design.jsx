e80480a00409f5ce740c4ee4eaf6d926c649e579
Kandwanahonathan <nathankandwanaho3@gmail.com>
2025-10-23 10:20:30 +0200

    my style of mi chat

--- my-app/src/chat-deign/chat.design.jsx
+++ my-app/src/chat-deign/chat.design.jsx
@@ -8,9 +8,13 @@ const Chat=()=>{
                 <h4>Live Chat</h4>
                 </div>
                
-            <div className="input">
-                <input type="text" placeholder=""/>
-                <button>Send</button>
+            <div className="md-5 col-5">
+                <form action="" className="d-flex align-items-center form-container ">
+                <input type="text" placeholder="!type your message ......." className="form-control me-2"/>
+                <button className="btn btn-success">
+                    <i className="bi bi-send-fill"></i>
+                </button>
+                </form>
             </div>
             </center>
 
