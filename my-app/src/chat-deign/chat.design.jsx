5d21fbb5af8af306b1c9303f3001f4e5819d8a07
Kandwanahonathan <nathankandwanaho3@gmail.com>
2025-10-23 13:26:51 +0200

    mi deign

--- my-app/src/chat-deign/chat.design.jsx
+++ my-app/src/chat-deign/chat.design.jsx
@@ -1,20 +1,44 @@
 import React,{useState,useEffect} from "react";
+import { FaPaperPlane } from "react-icons/fa";
+
 const Chat=()=>{
+    const [message,setMessage] =useState("");
+    const messages=([""])
     return(
         
         <div className="App">
             <center>
-            <div className="head">
-                <h4>Live Chat</h4>
+            <div className="head mt-3  border rounded col-md-5  bg-dark" style={{backgroundColor:""}}>
+                <h4 className="text-white fw-bold fst-italic">Live Chat</h4>
                 </div>
-               
-            <div className="md-5 col-5">
+               <div className="chat-box col-md-5 col-ms-10">
+             {/* message box */}
+                <div className="border rounded p-3 mb-3 shadow-m"
+                style={{
+                    height:"300px",
+                    backgroundColor:"#f8f9fa",
+                    overflowY:"auto",
+                }}
+                
+                >
+
+
+                </div>
+
+               </div>
+            <div className="col-md-5 col-sm-10">
                 <form action="" className="d-flex align-items-center form-container ">
-                <input type="text" placeholder="!type your message ......." className="form-control me-2"/>
-                <button className="btn btn-success">
+                <input type="text" value={message} placeholder="!type your message ......" 
+                className="form-control me-2"
+
+                onChange={(e)=>setMessage(e.target.value)}
+                
+                />
+                <button className="btn btn-success" onClick={add}>
                     <i className="bi bi-send-fill"></i>
                 </button>
                 </form>
+                <p className="mt-3"><strong>{message}</strong></p>
             </div>
             </center>
 
@@ -23,4 +47,4 @@ const Chat=()=>{
         </div>
     )
 }
-export default Chat
+export default Chat 
